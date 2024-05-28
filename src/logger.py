d011702ffbb10235a4e672f6f36c6b10419cbc03
Fareesuz Zama <fareesuzzama@gmail.com>
2024-05-28 11:31:59 +0530

    basic structure

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,10 @@ import logging
 import os
 from datetime import datetime
 
-# Define temporary directory path (modify if needed)
-TMP_DIR = "/tmp/logs"
-
-# Create timestamped log filename
+TMP_DIR = "D:/Projects/mlpredvalue/logs"
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
-# Combine path and filename for log location
 logs_path = os.path.join(TMP_DIR, LOG_FILE)
-
-# Create the log directory with exist_ok=True to avoid errors if it exists
 os.makedirs(logs_path, exist_ok=True)
-
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
